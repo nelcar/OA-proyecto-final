5f5ae43425cc80686eb81947a453373959996b5e
Nelson Cardenas <nelsonjc.01@gmail.com>
2015-11-04 22:04:24 -0600

    Menu Creado

--- main.cpp
+++ main.cpp
@@ -3,158 +3,128 @@
  * Author:  Nelson Cardenas
  *
  * Created on 04 de noviembre de 2015, 09:26 AM
- *
+ */
 
 #include <cstdlib>
+#include <iostream>
 
 using namespace std;
 
 /*
  * 
- *
+ */
+int menuPrincipal();
+int menuArchivo();
+int menuCampos();
+int menuRegistros();
+int menuIndices();
+int menuEstandarizacion();
+
 int main(int argc, char** argv) {
-    
-    return 0;
-}
-*/
-#include <windows.h>
-#include <stdlib.h>
-#include <string.h>
-#include <tchar.h>
-
-// Global variables
-
-// The main window class name.
-static TCHAR szWindowClass[] = _T("win32app");
-
-// The string that appears in the application's title bar.
-static TCHAR szTitle[] = _T("Win32 Guided Tour Application");
-
-HINSTANCE hInst;
-
-// Forward declarations of functions included in this code module:
-LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
-
-int WINAPI WinMain(HINSTANCE hInstance,
-                   HINSTANCE hPrevInstance,
-                   LPSTR lpCmdLine,
-                   int nCmdShow)
-{
-    WNDCLASSEX wcex;
-
-    wcex.cbSize = sizeof(WNDCLASSEX);
-    wcex.style          = CS_HREDRAW | CS_VREDRAW;
-    wcex.lpfnWndProc    = WndProc;
-    wcex.cbClsExtra     = 0;
-    wcex.cbWndExtra     = 0;
-    wcex.hInstance      = hInstance;
-    wcex.hIcon          = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_APPLICATION));
-    wcex.hCursor        = LoadCursor(NULL, IDC_ARROW);
-    wcex.hbrBackground  = (HBRUSH)(COLOR_WINDOW+1);
-    wcex.lpszMenuName   = NULL;
-    wcex.lpszClassName  = szWindowClass;
-    wcex.hIconSm        = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_APPLICATION));
-
-    if (!RegisterClassEx(&wcex))
-    {
-        MessageBox(NULL,
-            _T("Call to RegisterClassEx failed!"),
-            _T("Win32 Guided Tour"),
-            NULL);
-
-        return 1;
+    int opcion = 0;
+    opcion = menuPrincipal();
+    while (opcion < 1 || opcion > 5) {
+        opcion = menuPrincipal();
     }
-
-    hInst = hInstance; // Store instance handle in our global variable
-
-    // The parameters to CreateWindow explained:
-    // szWindowClass: the name of the application
-    // szTitle: the text that appears in the title bar
-    // WS_OVERLAPPEDWINDOW: the type of window to create
-    // CW_USEDEFAULT, CW_USEDEFAULT: initial position (x, y)
-    // 500, 100: initial size (width, length)
-    // NULL: the parent of this window
-    // NULL: this application does not have a menu bar
-    // hInstance: the first parameter from WinMain
-    // NULL: not used in this application
-    HWND hWnd = CreateWindow(
-        szWindowClass,
-        szTitle,
-        WS_OVERLAPPEDWINDOW,
-        CW_USEDEFAULT, CW_USEDEFAULT,
-        500, 100,
-        NULL,
-        NULL,
-        hInstance,
-        NULL
-    );
-
-    if (!hWnd)
-    {
-        MessageBox(NULL,
-            _T("Call to CreateWindow failed!"),
-            _T("Win32 Guided Tour"),
-            NULL);
-
-        return 1;
+    if (opcion == 1) {
+        opcion = menuArchivo();
+        while (opcion < 1 || opcion > 5) {
+            opcion = menuArchivo();
+        }
     }
-
-    // The parameters to ShowWindow explained:
-    // hWnd: the value returned from CreateWindow
-    // nCmdShow: the fourth parameter from WinMain
-    ShowWindow(hWnd,
-        nCmdShow);
-    UpdateWindow(hWnd);
-
-    // Main message loop:
-    MSG msg;
-    while (GetMessage(&msg, NULL, 0, 0))
-    {
-        TranslateMessage(&msg);
-        DispatchMessage(&msg);
+    if (opcion == 2) {
+        opcion = menuCampos();
+        while(opcion < 1 || opcion > 5){
+            opcion = menuCampos();
+        }
     }
-
-    return (int) msg.wParam;
-}
-
-//
-//  FUNCTION: WndProc(HWND, UINT, WPARAM, LPARAM)
-//
-//  PURPOSE:  Processes messages for the main window.
-//
-//  WM_PAINT    - Paint the main window
-//  WM_DESTROY  - post a quit message and return
-//
-//
-LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
-{
-    PAINTSTRUCT ps;
-    HDC hdc;
-    TCHAR greeting[] = _T("Hello, World!");
-
-    switch (message)
-    {
-    case WM_PAINT:
-        hdc = BeginPaint(hWnd, &ps);
-
-        // Here your application is laid out.
-        // For this introduction, we just print out "Hello, World!"
-        // in the top left corner.
-        TextOut(hdc,
-            5, 5,
-            greeting, _tcslen(greeting));
-        // End application-specific layout section.
-
-        EndPaint(hWnd, &ps);
-        break;
-    case WM_DESTROY:
-        PostQuitMessage(0);
-        break;
-    default:
-        return DefWindowProc(hWnd, message, wParam, lParam);
-        break;
+    if (opcion == 3) {
+        opcion = menuRegistros();
+        while(opcion < 1 || opcion > 6){
+            opcion = menuRegistros();
+        }
     }
-
+    if (opcion == 4) {
+        opcion = menuIndices();
+        while(opcion < 1 || opcion > 3){
+            opcion = menuIndices();
+        }
+    }
+    if (opcion == 5) {
+        opcion = menuEstandarizacion();
+        while(opcion < 1 || opcion > 4){
+            opcion = menuEstandarizacion();
+        }
+    }
     return 0;
 }
 
+int menuPrincipal() {
+    int opcion = 0;
+    cout << "****MENU PRINCIPAL****" << endl;
+    cout << "1. Archivo" << endl;
+    cout << "2. Campos" << endl;
+    cout << "3. Registros" << endl;
+    cout << "4. Índices" << endl;
+    cout << "5. Estandarización" << endl;
+    cin >> opcion;
+    return opcion;
+}
+
+int menuArchivo() {
+    int opcion = 0;
+    cout << "****MENU ARCHIVO****" << endl;
+    cout << "1. Nuevo Archivo" << endl;
+    cout << "2. Salvar Archivo" << endl;
+    cout << "3. Cerrar Archivo" << endl;
+    cout << "4. Salir" << endl;
+    cout << "5. VOLVER AL MENU PRINCIPAL" << endl;
+    cin >> opcion;
+    return opcion;
+}
+
+int menuCampos() {
+    int opcion = 0;
+    cout << "****MENU CAMPOS****" << endl;
+    cout << "1. Crear Campos" << endl;
+    cout << "2. Listar Campos" << endl;
+    cout << "3. Modificar Campos" << endl;
+    cout << "4. Borrar Campos" << endl;
+    cout << "5. VOLVER AL MENU PRINCIPAL" << endl;
+    cin >> opcion;
+    return opcion;
+}
+
+int menuRegistros() {
+    int opcion = 0;
+    cout << "****MENU REGISTROS****" << endl;
+    cout << "1. Introducir Registros" << endl;
+    cout << "2. Modificar Registros" << endl;
+    cout << "3. Buscar Registros" << endl;
+    cout << "4. Borrar Registros" << endl;
+    cout << "5. Listar Registros" << endl;
+    cout << "6. VOLVER AL MENU PRINCIPAL" << endl;
+    cin >> opcion;
+    return opcion;
+}
+
+int menuIndices() {
+    int opcion = 0;
+    cout << "****MENU ÍNDICES****" << endl;
+    cout << "1. Crear Índices" << endl;
+    cout << "2. Re Indexar Archivos" << endl;
+    cout << "3. VOLVER AL MENU PRINCIPAL" << endl;
+    cin >> opcion;
+    return opcion;
+}
+
+int menuEstandarizacion() {
+    int opcion = 0;
+    cout << "****MENU ESTANDARIZACION****" << endl;
+    cout << "1. Exportar Excel" << endl;
+    cout << "2. Exportar XML con Schema" << endl;
+    cout << "3. Exportar JSON" << endl;
+    cout << "4. VOLVER AL MENU PRINCIPAL" << endl;
+    cin >> opcion;
+    return opcion;
+}
